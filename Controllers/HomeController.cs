179a699b85cb9072a41c7b949d02a9b3ef85dfc3
Tisianer <45898939+Tisianer@users.noreply.github.com>
2020-02-26 09:29:35 +0400

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,14 +17,6 @@ namespace DailyPlanner.Controllers
         List<string> TypesEntries = new List<string>() { null, "Встреча", "Дело", "Памятка" };
         List<string> ModesDisplay = new List<string>() { "Все время", "День", "Неделя", "Месяц" };
 
-        private DateTime TryParseDate(string strDate)
-        {
-            bool Date_fr_FR = DateTime.TryParse(strDate, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dt_fr_FR);
-            if (!Date_fr_FR)
-                ModelState.AddModelError("StartDate", "Дата и время должны быть формата \"дд/мм/гггг\" или \"дд.мм.гггг\"!");
-            return dt_fr_FR;
-        }
-
         private Diary TryParseDate(Diary diary, string strStartDate, string strEndDate)
         {
             bool startDate_fr_FR = DateTime.TryParseExact(strStartDate, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime StartDate_fr_FR);
@@ -57,34 +49,38 @@ namespace DailyPlanner.Controllers
         {
             //Фильтрации.
             int pageSize = 2; // количество объектов на страницу.
-            DateTime Date = TryParseDate(start_date);
+            bool Date_fr_FR = DateTime.TryParseExact(start_date, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dt_fr_FR);
+            bool Date_de_DE = DateTime.TryParseExact(start_date, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dt_de_DE);
+            DateTime Date = Date_fr_FR == true ? dt_fr_FR : dt_de_DE;
             IQueryable<Diary> diarys = db.Diarys;
             if (!String.IsNullOrEmpty(type_entries))
                 diarys = diarys.Where(type => type.TypeEntries == type_entries); // Выборка по заданному типу.
             if (!String.IsNullOrEmpty(mode_display))
             {
                 if (mode_display == "День")
-                    diarys = diarys.Where(s => s.StartDate == DateTime.Today); // Выборка по текущему дню.
+                    diarys = diarys.Where(s => DbFunctions.TruncateTime(s.StartDate) == DateTime.Today.Date); // Выборка по текущему дню.
                 else if (mode_display == "Неделя")
                 {
                     var StartWeek = DateTime.Today.AddDays(-((double)DateTime.Today.DayOfWeek - 1));//получение даты начала недели.
                     var EndtWeek = DateTime.Today.AddDays(+((double)DateTime.Today.DayOfWeek - 1));//получение даты конца недели.
-                    diarys = diarys.Where(s => s.StartDate >= StartWeek && s.StartDate <= EndtWeek); // Выборка по текущей неделе.
+                    diarys = diarys.Where(s => DbFunctions.TruncateTime(s.StartDate) >= StartWeek.Date && DbFunctions.TruncateTime(s.StartDate) <= EndtWeek.Date); // Выборка по текущей неделе.
                 }
                 else if (mode_display == "Месяц")
                     diarys = diarys.Where(s => s.StartDate.Month == DateTime.Today.Month && s.StartDate.Year == DateTime.Today.Year); // Выборка по текущему месяцу и году
             }
-            if (!String.IsNullOrEmpty(start_date))
+            if (!String.IsNullOrEmpty(start_date) && (Date_fr_FR || Date_de_DE))
             {
-                diarys = diarys.Where(s => s.StartDate == Date); // Выборка по заданной дате.
+                diarys = diarys.Where(s => DbFunctions.TruncateTime(s.StartDate) == Date.Date); // Выборка по заданной дате.
             }
+            else
+                ModelState.AddModelError("StartDate", "Дата и время должны быть формата \"дд/мм/гггг\" или \"дд.мм.гггг\"!");
             if (!String.IsNullOrEmpty(search_string))
                 diarys = diarys.Where(top => top.Topic.ToUpper().Contains(search_string.ToUpper())); // Поиск по заголовку.
             // Пагинация.
             PageInfo pageInfo = new PageInfo(page, pageSize, diarys.Count());
             //Собирание модели.
             viewModel.DiarysListFilter = new DiarysListFilter ( TypesEntries, ModesDisplay, search_string,
-                (String.IsNullOrEmpty(start_date) == true ? null : Date.ToString("dd.MM.yyyy")), //для отображения даты без времени при пагинации.
+                ((Date_fr_FR || Date_de_DE)  ? Date.ToString("dd.MM.yyyy") : null), //для отображения даты без времени при пагинации.
                 type_entries, mode_display );
             viewModel.PageInfo = pageInfo;
             viewModel.Diarys = diarys.ToList().Skip((page - 1) * pageSize).Take(pageSize); // Подсчет пропускаемых объектов на страницу пагинации.
